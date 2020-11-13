0faceb1a0a3726de822ade0f27bd4a0e82e3a6c7
nicholas919 <64128376+nicholas919@users.noreply.github.com>
2020-11-13 10:54:21 +0700

    update

--- app.js
+++ app.js
@@ -220,7 +220,6 @@ createForm.addEventListener('submit', (e) => {
 })
 
 function auto_grow(element){
-    element.style.height = "5px";
     element.style.height = (element.scrollHeight)+"px";
 }
 
